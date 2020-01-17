8d769dad9e27a5509bcf03a9a7608a60ac603d46
Marco <marcopa84@hotmail.it>
2020-01-17 16:43:07 +0100

    VERSIONE FUNZIONANTE

---
+++ main.js
@@ -0,0 +1,52 @@
+$(document).ready(
+  function () {
+    $('.next').click(
+      function () {
+        clickNext();
+      }
+    )
+    $('.prev').click(
+      function () {
+        clickPrev();
+      }
+    )
+  }
+);
+
+function clickNext () {
+  var imageActive = $('img.active');
+  var imageNext = imageActive.next();
+  var circleActive = $('i.active');
+  var circleNext = circleActive.next();
+
+  if (imageActive.hasClass('last') == true){
+    imageActive.removeClass('active')
+    $('img.first').addClass('active');
+    circleActive.removeClass('active');
+    $('i.first').addClass('active');
+  } else {
+    imageActive.removeClass('active');
+    imageNext.addClass('active');
+    circleActive.removeClass('active');
+    circleNext.addClass('active');
+  }
+
+}
+function clickPrev () {
+  var imageActive = $('img.active');
+  var imagePrev = imageActive.prev();
+  var circleActive = $('i.active');
+  var circlePrev = circleActive.prev();
+
+  if (imageActive.hasClass('first') == true){
+    imageActive.removeClass('active')
+    $('img.last').addClass('active');
+    circleActive.removeClass('active');
+    $('i.last').addClass('active');
+  } else {
+    imageActive.removeClass('active');
+    imagePrev.addClass('active');
+    circleActive.removeClass('active');
+    circlePrev.addClass('active');
+  }
+}
